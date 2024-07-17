8a98110ca14e0169aca1bdabb12ff27128380eb8
Serhii Smirnov <free.smilesrg@gmail.com>
2024-07-17 20:32:10 +0200

    Command for rates synchronizer

---
+++ src/Command/SynchronizeRatesCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Service\Currency\Synchronizer\RatesSynchronizer;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'app:synchronize:rates', description: 'Synchronize all currencies rates')]
+class SynchronizeRatesCommand extends Command
+{
+    public function __construct(
+        private readonly RatesSynchronizer $ratesSynchronizer,
+    ){
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->ratesSynchronizer->synchronize();
+
+        $output->writeln('Rates synchronized!');
+
+        return Command::SUCCESS;
+    }
+}
